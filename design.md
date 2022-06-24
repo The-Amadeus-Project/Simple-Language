df1a0f0274e2106400af36b40dce73b18195e31f
CunningBard <jhonjhonjumaday@gmail.com>
2022-06-24 13:49:53 +0800

    philippines is init

---
+++ design.md
@@ -0,0 +1,13 @@
+# syntax
+
+```cpp
+int name 123; // no expression in val but func returns are goo
+// int name eval("123 - 843");
+
+out(name); // prints name
+
+// no -= += \= *=
+minus = eval(f"{name} + 123");
+
+out(minus);
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod lexer;
 
 fn main() {
-    println!("Hello, world!");
+    let file = "main.sl";
+    let content= std::fs::read_to_string(file).expect("couldnt open file");
+    let mut the_lexer = lexer::Lexer::new();
+    let res = the_lexer.lex_text(content);
+
 }

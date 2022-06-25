d84106a1cf3af5f5f7724d133e781e92dc86c07b
Eric <jhonjhonjumaday@gmail.com>
2022-06-25 17:15:56 +0800

    parser test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+extern crate core;
+
 mod lexer;
+mod parser;
+mod ast;
 
 fn main() {
     let file = "main.sl";
     let content= std::fs::read_to_string(file).expect("couldnt open file");
-    let mut the_lexer = lexer::Lexer::new();
-    let res = the_lexer.lex_text(content);
+    let mut the_parser = parser::Parser::new();
+    let parsed = the_parser.parse_text(content);
+    for part in parsed {
+        println!("{:?}", part);
+    }
 
 }

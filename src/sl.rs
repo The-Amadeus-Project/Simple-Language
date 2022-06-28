e3d3ce2c31dd385d2e66ed7938df7b69ca15286e
CunningBard <jhonjhonjumaday@gmail.com>
2022-06-28 22:03:14 +0800

    added precompile function to lessen lines of code

--- src/sl.rs
+++ src/sl.rs
@@ -1,11 +1,9 @@
+use eval::eval;
 use crate::{Parsed, parser};
 use crate::check::Checker;
+use crate::interpreter::Interpreter;
 
-pub fn compile(file_path: String) {
-    unimplemented!()
-}
-
-pub fn interpret(file_path: String) {
+fn pre_compile(file_path: String) -> Parsed {
     let content= std::fs::read_to_string(file_path).expect("couldnt open file");
     let mut the_parser = parser::Parser::new();
     let return_parsed = the_parser.parse_text(content);
@@ -16,5 +14,17 @@ pub fn interpret(file_path: String) {
     }
     println!("--------------------------------------------------------");
     let mut the_checker = Checker::new();
-    the_checker.check_program(return_parsed.clone())
+    the_checker.check_program(return_parsed.clone());
+    return_parsed
+}
+
+pub fn compile(file_path: String) {
+    unimplemented!()
+}
+
+pub fn interpret(file_path: String) {
+    let ret = pre_compile(file_path);
+
+    let mut master = Interpreter::new(ret);
+    master.run();
 }

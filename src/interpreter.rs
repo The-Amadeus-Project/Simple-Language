33cc9b27c0e7016b57f45fb734cb8f9bdeca8cf7
CunningBard <jhonjhonjumaday@gmail.com>
2022-06-29 03:52:25 +0800

    added built in funcs, handler for reassignment and builtin functions

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -20,6 +20,7 @@ pub struct Interpreter {
     program: Vec<Parsed>,
     defined_var: HashMap<String, (VarTypes, Value)>,
     defined_func: HashMap<String, (u32, Vec<VarTypes>)>,
+    built_in_func: Vec<String>,
     defined_struct: HashMap<String, ()>
 }
 
@@ -30,6 +31,7 @@ impl Interpreter {
                 program,
                 defined_var: HashMap::new(),
                 defined_func: HashMap::new(),
+                built_in_func: vec!["iprint".to_string(), "fprint".to_string(), "bprint".to_string(), "sprint".to_string(), ],
                 defined_struct: HashMap::new()
             }
         } else {
@@ -55,7 +57,7 @@ impl Interpreter {
                     Value::String(val) => to_eval += &val,
                     Value::Int(val) => to_eval += &val.to_string(),
                     Value::Float(val) => to_eval += &val.to_string(),
-                    _ => panic!("HOW?!!!")
+                    Value::Bool(val) => to_eval += &val.to_string(),
                 }
             } else {
                 unimplemented!()
@@ -63,6 +65,27 @@ impl Interpreter {
         }
         eval::eval(&to_eval).expect("sir! you got a shitty error").to_string()
     }
+    fn var_reassignment(&mut self, var_name: Token, var_value: Vec<Token>){
+        let mut reassign = self.defined_var.remove(&var_name.value).unwrap();
+        self.defined_var.insert(var_name.value.clone(), reassign.clone());
+        let var_type = reassign.0.clone();
+        match var_type {
+            VarTypes::Str => {
+                unimplemented!()
+            },
+            VarTypes::Int => {
+                let ret_eval = self.evaluate(var_value).parse::<i128>().expect("sir! you got another shitty error");
+                self.defined_var.insert(var_name.value, (var_type, Value::Int(ret_eval)));
+            },
+            VarTypes::Float => {
+                unimplemented!()
+            },
+            VarTypes::Bool => {
+                unimplemented!()
+            }
+        }
+    }
+
     fn var_assignment(&mut self, var_name: Token, var_type: VarTypes, var_value: Vec<Token>){
         match var_type {
             VarTypes::Str => {
@@ -80,12 +103,51 @@ impl Interpreter {
             }
         }
     }
+    fn built_in_funcs(&mut self, func_name: Token, func_args: Vec<Token>) -> Option<Value>{
+        match &*func_name.value {
+            "iprint" => {
+                let arg = func_args[0 as usize].clone();
+                if arg.is_integer(){
+                    println!("{}", arg.value)
+                } else if arg.token_type == TokenType::Identifier {
+                    let arg_name = arg.value;
+                    if self.defined_var.contains_key(&arg_name){
+                        let referred = self.defined_var.get(&arg_name).unwrap();
+                        match referred.1 {
+                            Value::Int(val) => println!("{}", val),
+                            _ => panic!("type checker failed me")
+                        }
+                    } else {
+                        unimplemented!("not yet '{}'", func_name.value)
+                    }
+                } else {
+                    panic!("how did you get here?")
+                }
+            }
+            _ => unimplemented!("not yet '{}'", func_name.value)
+        }
+        None
+    }
+    fn func_call(&mut self, func_name: Token, func_args: Vec<Token>){
+        if self.built_in_func.contains(&func_name.value){
+            self.built_in_funcs(func_name, func_args);
+        } else {
+            unimplemented!()
+        }
+    }
     pub fn run(&mut self){
         let program = self.program.clone();
         for part in program {
             match part {
-                Parsed::VariableAssignment(var_name, var_type, var_value) =>
-                self.var_assignment(var_name.clone(), var_type.clone(), var_value.clone()),
+                Parsed::VariableAssignment(var_name, var_type, var_value) => {
+                    self.var_assignment(var_name.clone(), var_type.clone(), var_value.clone())
+                },
+                Parsed::FuncCall(func_name, func_args) => {
+                    self.func_call(func_name, func_args);
+                },
+                 Parsed::VariableReassignment(var_name, var_value) => {
+                    self.var_reassignment(var_name.clone(), var_value.clone())
+                },
                 _ => unimplemented!()
             }
         }

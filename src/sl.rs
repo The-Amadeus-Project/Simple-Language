368ee1a23d6da594a2ea4a87f076bc1fe3972761
Eric <jhonjhonjumaday@gmail.com>
2022-06-29 15:10:37 +0800

    testing on type parser

--- src/sl.rs
+++ src/sl.rs
@@ -2,6 +2,7 @@ use eval::eval;
 use crate::{Parsed, parser};
 use crate::check::Checker;
 use crate::interpreter::Interpreter;
+use crate::lexer::{Lexer, Token,TokenType };
 
 fn pre_compile(file_path: String) -> Parsed {
     let content= std::fs::read_to_string(file_path).expect("couldnt open file");
@@ -18,11 +19,57 @@ fn pre_compile(file_path: String) -> Parsed {
     return_parsed
 }
 
+struct TypeParser {
+    to_parse: Vec<Token>,
+    ind: i32,
+    current_token: Token
+}
+
+impl TypeParser {
+    fn new() -> Self {
+        Self {
+            to_parse: vec![],
+            ind: -1,
+            current_token: Token{
+                token_type: TokenType::NullForParser,
+                value: "".to_string(),
+                x: 0,
+                y: 0
+            }
+        }
+    }
+    
+    fn parse_tokens(&mut self, tokens: Vec<Token>){
+        self.to_parse = tokens;
+    }
+}
+
+fn type_parser(tokens: Vec<Token>){
+    let mut open = 0;
+    let mut type_parse = vec![];
+    for tok in tokens {
+        if tok.token_type == TokenType::ParenthesisOpen {
+            open += 1;
+        } else if tok.token_type == TokenType::ParenthesisClose {
+            open -= 1;
+            if open == 0 {
+                type_parser(type_parse.clone());
+                type_parse.clear();
+            }
+        }
+    }
+}
+
 pub fn compile(file_path: String) {
     unimplemented!()
 }
 
 pub fn interpret(file_path: String) {
+
+    let mut test = Lexer::new();
+    let d = test.lex_text(" 1 + 1 + (2 + 2)".to_string());
+    type_parser(d);
+    return;
     let ret = pre_compile(file_path);
 
     let mut master = Interpreter::new(ret);

8937ffaaf8381a520f7a250efe801838886fff91
CunningBard <jhonjhonjumaday@gmail.com>
2022-07-02 02:34:20 +0800

    added debug mode to parser

--- src/sl.rs
+++ src/sl.rs
@@ -7,16 +7,18 @@ use crate::parser::VarTypes;
 
 
 
-fn pre_compile(file_path: String) -> Parsed {
+fn pre_compile(file_path: String, debug: bool) -> Parsed {
     let file_content = std::fs::read_to_string(file_path).expect("couldnt open file");
-    let mut the_parser = parser::Parser::new();
+    let mut the_parser = parser::Parser::new(debug);
     let return_parsed = the_parser.parse_text(file_content);
-    if let Parsed::Program(parsed) = &return_parsed {
-        for part in parsed {
-            println!("{:?}", part)
+    if debug {
+        if let Parsed::Program(parsed) = &return_parsed {
+            for part in parsed {
+                println!("{:?}", part)
+            }
         }
+        println!("--------------------------------------------------------");
     }
-    println!("--------------------------------------------------------");
     let mut type_checker = Checker::new();
     type_checker.check_program(return_parsed.clone());
     return_parsed
@@ -27,8 +29,8 @@ pub fn compile(file_path: String) {
     unimplemented!()
 }
 
-pub fn interpret(file_path: String) {
-    let ret = pre_compile(file_path);
+pub fn interpret(file_path: String, debug: bool) {
+    let ret = pre_compile(file_path, debug);
 
     let mut master = Interpreter::new(ret);
     master.run();
